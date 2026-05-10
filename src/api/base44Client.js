bd0d36f85d7f07d9092a789d11894a3a1c58f90c
tranthitrucxinh23052005-debug <tranthitrucxinh23052005@gmail.com>
2026-05-10 14:23:09 +0700

    ver token

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,14 +1,14 @@
+// src/api/base44Client.js
 import { createClient } from '@base44/sdk';
 import { appParams } from '@/lib/app-params';
 
 const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
-// Trả về nguyên bản để SDK tự động trỏ về backend gcsystem.base44.app
 export const base44 = createClient({
   appId,
   token,
   functionsVersion,
-  serverUrl: '',
+  serverUrl: '', // Để trống để SDK dùng relative path
   requiresAuth: false,
-  appBaseUrl
+  appBaseUrl: '' // Để trống để SDK tự hiểu là đi từ domain hiện tại (Vercel) sau đó vercel.json sẽ proxy đi
 });
